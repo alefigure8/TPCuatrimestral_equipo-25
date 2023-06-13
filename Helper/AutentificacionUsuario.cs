99df57dcdf2568c35dc08ae711c1f6f70fe92109
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-06-13 16:05:55 -0300

    Agrego helper de autentificacion

---
+++ Helper/AutentificacionUsuario.cs
@@ -0,0 +1,25 @@
+﻿
+using Dominio;
+using Opciones;
+
+namespace Helper
+{
+	public class AutentificacionUsuario
+	{
+		static public bool esUser(Usuario user)
+		{
+			if (user == null)
+				return false;
+
+			return true;
+		}
+
+		static public bool esAdmin(Usuario user)
+		{
+			if (user.Tipo.Descripcion == ColumnasDB.TipoUsuario.Admin)
+				return true;
+
+			return false;
+		}
+	}
+}
