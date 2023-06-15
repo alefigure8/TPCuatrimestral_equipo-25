ffbf7036ac603495ab263ae2a1f5f7c31498bcbb
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-06-15 15:07:54 -0300

    Modifico Tipo de Usuriaro

--- Negocio/UsuarioNegocio.cs
+++ Negocio/UsuarioNegocio.cs
@@ -1,11 +1,13 @@
 ﻿using Dominio;
 using System;
 using Opciones;
+using System.Collections.Generic;
 
 namespace Negocio
 {
 	public class UsuarioNegocio
 	{
+		//Buscamos usuarios por mail y pass
 		public Usuario BuscarUsuario(string mail, string pass)
 		{
 
@@ -14,7 +16,10 @@ namespace Negocio
 
 			try
 			{
-				datos.setQuery($"SELECT {ColumnasDB.Usuario.Id}, {ColumnasDB.Usuario.Nombres}, {ColumnasDB.Usuario.Apellidos}, {ColumnasDB.Usuario.Mail}, {ColumnasDB.Usuario.Pass}, {ColumnasDB.Usuario.TipoUsuario} FROM {ColumnasDB.Usuario.DB} WHERE {ColumnasDB.Usuario.Mail} = '{mail}' AND {ColumnasDB.Usuario.Pass} = '{pass}'");
+				datos.setQuery($"SELECT {ColumnasDB.Usuario.Id}, {ColumnasDB.Usuario.Nombres}, {ColumnasDB.Usuario.Apellidos}, {ColumnasDB.Usuario.Mail}, {ColumnasDB.Usuario.Pass}, {ColumnasDB.TipoUsuario.Descripcion} " +
+					$"FROM {ColumnasDB.Usuario.DB} " +
+					$"INNER JOIN {ColumnasDB.TipoUsuario.DB} ON {ColumnasDB.Usuario.TipoUsuario} = {ColumnasDB.TipoUsuario.Id} " +
+					$"WHERE {ColumnasDB.Usuario.Mail} = '{mail}' AND {ColumnasDB.Usuario.Pass} = '{pass}'");
 				datos.executeReader();
 
 				while (datos.Reader.Read())
@@ -36,16 +41,11 @@ namespace Negocio
 
 					//PASS
 					if (datos.Reader[ColumnasDB.Usuario.Pass] != null)
-						usuario.Password = (string)datos.Reader[ColumnasDB.Usuario.Pass];
-
-
-					if (datos.Reader[ColumnasDB.Usuario.TipoUsuario] != null)
-					{
-						TipoUsuarios auxTipoUsuario = new TipoUsuarios();
-						TipoUsuariosNegocio tipoUsuariosNegocio = new TipoUsuariosNegocio();
-						auxTipoUsuario = tipoUsuariosNegocio.ListarPorId((Int32)datos.Reader[ColumnasDB.Usuario.Id]);
-						usuario.Tipo = auxTipoUsuario;
-					}
+						usuario.Password = (string)datos.Reader[ColumnasDB.Usuario.Pass];					
+					
+					//TIPO USUARIO
+					if (datos.Reader[ColumnasDB.TipoUsuario.Descripcion] != null)
+						usuario.Tipo = (string)datos.Reader[ColumnasDB.TipoUsuario.Descripcion];
 				}
 
 			}
@@ -59,6 +59,64 @@ namespace Negocio
 			}
 
 			return String.IsNullOrEmpty(usuario.Mail) ? null : usuario;
+		}
+
+		// Listar los usuarios por meseros
+		public List<Usuario> ListarMeseros()
+		{
+			List<Usuario> meseros = new List<Usuario>();
+			AccesoDB datos = new AccesoDB();
+
+			try
+			{
+				int IDTIPO = 3;
+				datos.setQuery($"SELECT {ColumnasDB.Usuario.Id}, {ColumnasDB.Usuario.Nombres}, {ColumnasDB.Usuario.Apellidos}, {ColumnasDB.Usuario.Mail}, {ColumnasDB.Usuario.Pass}, {ColumnasDB.TipoUsuario.Descripcion} " +
+					$"FROM {ColumnasDB.Usuario.DB} " +
+					$"INNER JOIN {ColumnasDB.TipoUsuario.DB} ON {ColumnasDB.Usuario.TipoUsuario} = {ColumnasDB.TipoUsuario.Id} " +
+					$"WHERE {ColumnasDB.Usuario.TipoUsuario} = {IDTIPO}");
+				datos.executeReader();
+
+				while (datos.Reader.Read())
+				{
+					Usuario usuario = new Usuario();
+					
+					//ID
+					usuario.Id = (Int32)datos.Reader[ColumnasDB.Usuario.Id];
+
+					//NOMBRES
+					if (datos.Reader[ColumnasDB.Usuario.Nombres] != null)
+						usuario.Nombres = (string)datos.Reader[ColumnasDB.Usuario.Nombres];
+
+					//APELLIDOS
+					if (datos.Reader[ColumnasDB.Usuario.Apellidos] != null)
+						usuario.Apellidos = (string)datos.Reader[ColumnasDB.Usuario.Apellidos];
+
+					//MAIL
+					if (datos.Reader[ColumnasDB.Usuario.Mail] != null)
+						usuario.Mail = (string)datos.Reader[ColumnasDB.Usuario.Mail];
+
+					//PASS
+					if (datos.Reader[ColumnasDB.Usuario.Pass] != null)
+						usuario.Password = (string)datos.Reader[ColumnasDB.Usuario.Pass];
+					
+					//TIPO USUARIO
+					if (datos.Reader[ColumnasDB.TipoUsuario.Descripcion] != null)
+						usuario.Tipo = (string)datos.Reader[ColumnasDB.TipoUsuario.Descripcion];
+
+					meseros.Add(usuario);
+				}
+			}
+			catch (Exception Ex)
+			{
+				throw Ex;
+			}
+			finally
+			{
+				datos.closeConnection();
+			}
+
+			return meseros.Count == 0 ? null : meseros;
+
 		}
 	}
 }
