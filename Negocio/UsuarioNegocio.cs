8f07f3cfa9aa1eca88df759c0159db4d056368b8
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-06-15 15:15:04 -0300

    Revert "Modifico Tipo de Usuriaro"
    This reverts commit ffbf7036ac603495ab263ae2a1f5f7c31498bcbb.

--- Negocio/UsuarioNegocio.cs
+++ Negocio/UsuarioNegocio.cs
@@ -1,13 +1,11 @@
 ﻿using Dominio;
 using System;
 using Opciones;
-using System.Collections.Generic;
 
 namespace Negocio
 {
 	public class UsuarioNegocio
 	{
-		//Buscamos usuarios por mail y pass
 		public Usuario BuscarUsuario(string mail, string pass)
 		{
 
@@ -16,70 +14,11 @@ namespace Negocio
 
 			try
 			{
-				datos.setQuery($"SELECT {ColumnasDB.Usuario.Id}, {ColumnasDB.Usuario.Nombres}, {ColumnasDB.Usuario.Apellidos}, {ColumnasDB.Usuario.Mail}, {ColumnasDB.Usuario.Pass}, {ColumnasDB.TipoUsuario.Descripcion} " +
-					$"FROM {ColumnasDB.Usuario.DB} " +
-					$"INNER JOIN {ColumnasDB.TipoUsuario.DB} ON {ColumnasDB.Usuario.TipoUsuario} = {ColumnasDB.TipoUsuario.Id} " +
-					$"WHERE {ColumnasDB.Usuario.Mail} = '{mail}' AND {ColumnasDB.Usuario.Pass} = '{pass}'");
+				datos.setQuery($"SELECT {ColumnasDB.Usuario.Id}, {ColumnasDB.Usuario.Nombres}, {ColumnasDB.Usuario.Apellidos}, {ColumnasDB.Usuario.Mail}, {ColumnasDB.Usuario.Pass}, {ColumnasDB.Usuario.TipoUsuario} FROM {ColumnasDB.Usuario.DB} WHERE {ColumnasDB.Usuario.Mail} = '{mail}' AND {ColumnasDB.Usuario.Pass} = '{pass}'");
 				datos.executeReader();
 
 				while (datos.Reader.Read())
 				{
-					//ID
-					usuario.Id = (Int32)datos.Reader[ColumnasDB.Usuario.Id];
-
-					//NOMBRES
-					if (datos.Reader[ColumnasDB.Usuario.Nombres] != null)
-						usuario.Nombres = (string)datos.Reader[ColumnasDB.Usuario.Nombres];
-
-					//APELLIDOS
-					if (datos.Reader[ColumnasDB.Usuario.Apellidos] != null)
-						usuario.Apellidos = (string)datos.Reader[ColumnasDB.Usuario.Apellidos];
-
-					//MAIL
-					if (datos.Reader[ColumnasDB.Usuario.Mail] != null)
-						usuario.Mail = (string)datos.Reader[ColumnasDB.Usuario.Mail];
-
-					//PASS
-					if (datos.Reader[ColumnasDB.Usuario.Pass] != null)
-						usuario.Password = (string)datos.Reader[ColumnasDB.Usuario.Pass];					
-					
-					//TIPO USUARIO
-					if (datos.Reader[ColumnasDB.TipoUsuario.Descripcion] != null)
-						usuario.Tipo = (string)datos.Reader[ColumnasDB.TipoUsuario.Descripcion];
-				}
-
-			}
-			catch (Exception Ex)
-			{
-				throw Ex;
-			}
-			finally
-			{
-				datos.closeConnection();
-			}
-
-			return String.IsNullOrEmpty(usuario.Mail) ? null : usuario;
-		}
-
-		// Listar los usuarios por meseros
-		public List<Usuario> ListarMeseros()
-		{
-			List<Usuario> meseros = new List<Usuario>();
-			AccesoDB datos = new AccesoDB();
-
-			try
-			{
-				int IDTIPO = 3;
-				datos.setQuery($"SELECT {ColumnasDB.Usuario.Id}, {ColumnasDB.Usuario.Nombres}, {ColumnasDB.Usuario.Apellidos}, {ColumnasDB.Usuario.Mail}, {ColumnasDB.Usuario.Pass}, {ColumnasDB.TipoUsuario.Descripcion} " +
-					$"FROM {ColumnasDB.Usuario.DB} " +
-					$"INNER JOIN {ColumnasDB.TipoUsuario.DB} ON {ColumnasDB.Usuario.TipoUsuario} = {ColumnasDB.TipoUsuario.Id} " +
-					$"WHERE {ColumnasDB.Usuario.TipoUsuario} = {IDTIPO}");
-				datos.executeReader();
-
-				while (datos.Reader.Read())
-				{
-					Usuario usuario = new Usuario();
-					
 					//ID
 					usuario.Id = (Int32)datos.Reader[ColumnasDB.Usuario.Id];
 
@@ -98,13 +37,17 @@ namespace Negocio
 					//PASS
 					if (datos.Reader[ColumnasDB.Usuario.Pass] != null)
 						usuario.Password = (string)datos.Reader[ColumnasDB.Usuario.Pass];
-					
-					//TIPO USUARIO
-					if (datos.Reader[ColumnasDB.TipoUsuario.Descripcion] != null)
-						usuario.Tipo = (string)datos.Reader[ColumnasDB.TipoUsuario.Descripcion];
 
-					meseros.Add(usuario);
+
+					if (datos.Reader[ColumnasDB.Usuario.TipoUsuario] != null)
+					{
+						TipoUsuarios auxTipoUsuario = new TipoUsuarios();
+						TipoUsuariosNegocio tipoUsuariosNegocio = new TipoUsuariosNegocio();
+						auxTipoUsuario = tipoUsuariosNegocio.ListarPorId((Int32)datos.Reader[ColumnasDB.Usuario.Id]);
+						usuario.Tipo = auxTipoUsuario;
+					}
 				}
+
 			}
 			catch (Exception Ex)
 			{
@@ -115,8 +58,7 @@ namespace Negocio
 				datos.closeConnection();
 			}
 
-			return meseros.Count == 0 ? null : meseros;
-
+			return String.IsNullOrEmpty(usuario.Mail) ? null : usuario;
 		}
 	}
 }
