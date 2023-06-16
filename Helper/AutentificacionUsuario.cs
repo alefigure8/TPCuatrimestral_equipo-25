2213a0a60eab0c1cd11ce5d0d08adb0fa10344c7
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-06-16 07:26:57 -0300

    Agrego validaciones de usuarios

--- Helper/AutentificacionUsuario.cs
+++ Helper/AutentificacionUsuario.cs
@@ -8,15 +8,31 @@ namespace Helper
 	{
 		static public bool esUser(Usuario user)
 		{
-			if (user == null)
-				return false;
+			if (user != null)
+				return true;
 
-			return true;
+			return false;
 		}
 
 		static public bool esAdmin(Usuario user)
 		{
-			if (user.Tipo == ColumnasDB.TipoUsuario.Admin)
+			if (user?.Tipo == ColumnasDB.TipoUsuario.Admin)
+				return true;
+
+			return false;
+		}
+
+		static public bool esGerente(Usuario user)
+		{
+			if (user?.Tipo == ColumnasDB.TipoUsuario.Gerente)
+				return true;
+
+			return false;
+		}
+
+		static public bool esMesero(Usuario user)
+		{
+			if (user?.Tipo == ColumnasDB.TipoUsuario.Mesero)
 				return true;
 
 			return false;
