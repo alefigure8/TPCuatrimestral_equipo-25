00b581eb158ece584b77bf1835fb455557ac2261
FmmerayoUTN <federico.merayo@alumnos.frgp.utn.edu.ar>
2023-07-03 09:54:26 -0300

    Agrego clase Estadopedido

--- Dominio/Pedido.cs
+++ Dominio/Pedido.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    internal class Pedido
+    public class Pedido
     {
-    }
+        Pedido()
+        {
+            Estadopedido = new EstadoPedido();
+            Estadopedido.Id = 1;
+        }
+        public int Id { get; set; }
+        public List<ProductoDelDia> Productosdeldia;
+        public EstadoPedido Estadopedido
+        {
+            get; set;
+        }
 }
