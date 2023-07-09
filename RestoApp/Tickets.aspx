d00a34fbdd7975d7743054082a8a05f3c6bebead
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-07-09 11:04:55 -0300

    Agrego Ticket y modifico Métodos de negocio

---
+++ RestoApp/Tickets.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masters/Main.Master" AutoEventWireup="true" CodeBehind="Tickets.aspx.cs" Inherits="RestoApp.Tickets" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="row">
+        <header class="col-10 bg-white rounded p-3 m-3">
+            <p class="fs-2">Tickets</p>
+        </header>
+    </div>
+
+    <div class="row d-flex">
+        <!-- Repeater de Tickets-->
+        <asp:Repeater runat="server" ID="repeaterTickets">
+            <ItemTemplate>
+
+                <!-- Ticket -->
+                <section class="col-3 bg-white rounded d-flex flex-column m-3 p-3 border">
+                    <div class="d-flex flex-column w-100">
+                        <p class="fw-semibold fs-5 text-center">#<%# Eval("Id") %></p>
+                        <div class="d-flex justify-content-between w-100 border-1 border-bottom">
+                            <p><i class="fa-sharp fa-solid fa-calendar-days me-2"></i><%# Eval("Fecha", "{0:dd/MM/yyyy}") %></p>
+                            <p><i class="fa-regular fa-clock me-2"></i><%# Eval("Cierre") %></p>
+                        </div>
+                    </div>
+                    <div class="w-100 d-flex justify-content-between mt-3">
+                        <p><span class="fw-semibold">Mesa:</span> <%# Eval("Mesa") %></p>
+                        <p><span class="fw-semibold">Mesero:</span> <%# Eval("IdMesero") %></p>
+                    </div>
+
+                    <table class="table table-striped border">
+                        <thead class="table-secondary">
+                            <tr class="border">
+                                <th scope="col">#</th>
+                                <th scope="col">Pedido</th>
+                                <th scope="col">Cantidad</th>
+                                <th scope="col">Precio</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                    <%# RenderDetalles(Eval("Detalle")) %>
+                     </tbody>
+                    </table>
+
+                    <div class="border-top w-100 pt-3">
+                        <p><span class="fw-semibold">Total: $</span> <%= precio %></p>
+                        <button class="btn btn-dark w-100">Cobrar</button>
+                    </div>
+                </section>
+
+                <!-- Fin Ticket -->
+
+            </ItemTemplate>
+        </asp:Repeater>
+        <!-- Fin Repeater de Tickets-->
+
+    </div>
+</asp:Content>
---
+++ RestoApp/Tickets.aspx.cs
@@ -0,0 +1,159 @@
+﻿using Dominio;
+using Helper;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RestoApp
+{
+	public partial class Tickets : System.Web.UI.Page
+	{
+		private Usuario usuario;
+		public Decimal precio = 0;
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			//Verificar que sea usuario
+			if (AutentificacionUsuario.esUser(Helper.Session.GetUsuario()))
+				usuario = Helper.Session.GetUsuario();
+
+			//Verificar Query del número de servicio
+			int servicio = Convert.ToInt32(Request.QueryString["servicio"]);
+
+			// CONTENIDO GERENTE
+			if (!IsPostBack && AutentificacionUsuario.esGerente(usuario))
+			{
+				if (servicio > 0)
+				{
+					//Guardamos el número en session
+					Session["ServicioTicket"] = servicio;
+					BuscarTicket();
+				}
+				else
+				{
+					//Render de todos los tickets del mesero
+					CargarTicketsAbiertos();
+				}
+			}
+
+			// CONTENIDO GERENTE
+			if (!IsPostBack && AutentificacionUsuario.esMesero(usuario))
+			{
+
+				if (servicio > 0)
+				{
+					//Guardamos el número en session
+					Session["ServicioTicket"] = servicio;
+					BuscarTicket();
+				}
+				else
+				{
+					//Render de todos los tickets del mesero
+					CargarTicketPorMesero();
+				}
+
+			}
+
+			//Cargamos el repeater
+			CargarRepeaterTicker();
+
+		}
+
+		private List<Ticket> BuscarTicket()
+		{
+			//Iniciamos Ticket Negocio
+			TicketNegocio ticketNegocio = new TicketNegocio();
+
+			List<Ticket> tickets = new List<Ticket>();
+
+			//Obtenemos el ticket según el número de servcio
+			Ticket ticket = ticketNegocio.TicketPorServicio((int)Session["ServicioTicket"]);
+			
+			tickets.Add(ticket);
+			
+			//Guardamos ticket en session
+			Session["Tickets"] = tickets;
+
+			//Validamos que el mesero sea el mismo que el del ticket
+			int idMesero = Helper.Session.GetUsuario().Id;
+			
+			if(ticket.IdMesero == idMesero)
+			{
+				return tickets;
+			}
+
+			return null;
+		}
+
+		//Retornamos todos los ticket abiertos
+		private List<Ticket> CargarTicketsAbiertos()
+		{
+			//Iniciamos Ticket Negocio
+			TicketNegocio ticketNegocio = new TicketNegocio();
+
+			//Listamos tickets
+			List<Ticket> tickets = ticketNegocio.Listar();
+
+			//Guardamos Tickets en Session
+			Session["Tickets"] = tickets;
+
+			return tickets;
+		}
+
+		//Retornamos el listado de tickets según el mesero
+		private List<Ticket> CargarTicketPorMesero()
+		{
+			//Buscamos el id del mesero
+			int idMesero = Helper.Session.GetUsuario().Id;
+
+			//Iniciamos Ticket Negocio
+			TicketNegocio ticketNegocio = new TicketNegocio();
+
+			//Listamos tickets
+			List<Ticket> tickets = ticketNegocio.ListarPorMesero(idMesero);
+
+			return tickets;
+		}
+
+		private void CargarRepeaterTicker()
+		{
+			repeaterTickets.DataSource = (List<Ticket>)Session["Tickets"];
+			repeaterTickets.DataBind();
+		}
+
+		protected string RenderDetalles(object detallesObj)
+		{
+			if (detallesObj is List<TicketDetalle> detalles)
+			{
+				precio = 0;
+				StringBuilder sb = new StringBuilder();
+
+				foreach (TicketDetalle item in detalles)
+				{
+					precio += item.Precio;
+
+					sb.AppendFormat(
+						@"<tr>
+						<th scope=""row"">1</th>
+						<td>{0}</td>
+						<td>{1}</td>
+						<td>{2}</td>
+						</tr>",
+						item.Descripcion,
+						item.Cantidad,
+						item.Precio
+					);
+				}
+
+				return sb.ToString();
+			}
+
+			return string.Empty;
+		}
+
+	}
+}
