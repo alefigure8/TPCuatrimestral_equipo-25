ffe1726c58de17f54d3454eae9b41af4d3257a3a
FmmerayoUTN <federico.merayo@alumnos.frgp.utn.edu.ar>
2023-07-13 17:55:28 -0300

    Corrijo Metodo Cambio de Estado

--- Negocio/PedidoNegocio.cs
+++ Negocio/PedidoNegocio.cs
@@ -422,52 +422,22 @@ namespace Negocio
 
         }
 
-        public DateTime HorarioEnPrepPedido(int Pedido)
-        {
-            AccesoDB dB = new AccesoDB();
-            DateTime horario = new DateTime();
-
-            try
-            {
-                dB.setQuery($"Select FechaActualizacion from Estado_x_Pedido where IdPedido = {Pedido} and IdEstado = 2");
-                dB.executeReader();
-                while (dB.Reader.Read())
-                {
-
-                    horario = (DateTime)dB.Reader[ColumnasDB.EstadosxPedido.FechaActualizacion];
-
-                }
-                dB.closeConnection();
-            }
-            catch
-            {
-
-
-            }
-            finally
-            {
-                dB.closeConnection();
-
-            }
-
-            return horario;
-        }
-
-
-
+     
+        
         public void CambiarEstadoPedido(int idpedido, int nuevoestado, DateTime Fechahora)
         {
             AccesoDB datos = new AccesoDB();
             try
             {
+                string formato = Fechahora.ToString("yyyy-MM-dd HH:mm:ss");
 
                 datos.setQuery(
                  $"INSERT INTO {ColumnasDB.EstadosxPedido.DB} ({ColumnasDB.EstadosxPedido.IdPedido}, {ColumnasDB.EstadosxPedido.IdEstado}, {ColumnasDB.EstadosxPedido.FechaActualizacion}) " +
-                 $"VALUES ({idpedido}, {nuevoestado} , '{Fechahora}')");
+                 $"VALUES ({idpedido}, {nuevoestado} , CAST('{formato}' AS DATETIME))");
                 datos.executeNonQuery();
                 datos.closeConnection();
             }
-
+          
             catch (Exception Ex)
             {
 
